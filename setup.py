d6e9ce5582609356cb6d2797910e595fc19179c6
makolon <makoto.sato@weblab.t.u-tokyo.ac.jp>
2025-02-02 21:25:51 +0900

    誤植を修正

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xarm-eef-dynamixel-wrapper",  # Replace with your own username
+    name="xarm-eef-dynamixel-wrapper",
     version="0.0.1",
     install_requires=pathlib.Path("requirements.txt").read_text().splitlines(),
     author="Takuya Okubo",
-    description="A package to control Dynamixel servos attached to RS485 of end effector",
+    description="A package to control Dynamixel servos attached to RS485 of xArm/Lite6 end effector",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kzk-program/AIWolfK2B",
+    url="https://github.com/HoneyMack/xarm_eef_dynamixel_wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
